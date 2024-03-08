e91a44a28ca6c686220adebd72bc66a8ee2571c0
Matt Gaidica, Ph.D <matt@gaidi.ca>
2024-03-08 07:03:58 -0600

    Initial Commit

---
+++ APEX Acoustic Panel/APEX_Acoustic_PanelApp.swift
@@ -0,0 +1,17 @@
+//
+//  APEX_Acoustic_PanelApp.swift
+//  APEX Acoustic Panel
+//
+//  Created by Matt Gaidica on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct APEX_Acoustic_PanelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
